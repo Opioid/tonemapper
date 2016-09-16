46e4945ddd5225892a6bd83190ab14061d474a66
Benjamin Savs <Benjamin.SAVS@3ds.com>
2016-09-16 09:56:03 +0200

    Cleanup and add image

--- README.md
+++ README.md
@@ -6,3 +6,15 @@ It compares the following tonemappers:
 1. [Timothy Lottes Generic](http://32ipi028l5q82yhj72224m8j.wpengine.netdna-cdn.com/wp-content/uploads/2016/03/GdcVdrLottes.pdf)
 2. [John Hable "Uncharted" Filmic](http://filmicgames.com/archives/75Jo)
 3. [Krzysztof Narkowicz ACES approximation](https://knarkowicz.wordpress.com/2016/01/06/aces-filmic-tone-mapping-curve/)
+
+![Tonemapper comparison](https://github.com/Opioid/tonemapper/blob/master/tonemapper.png "Tonemapper comparison")
+
+The settings for the Generic Tonemapper are:
+
+- Contrast = 1
+- Shoulder = 1
+- Mid in   = 0.18
+- Mit out  = 0.18
+- HDR max  = 1
+
+Clearly, there is something wrong here...
--- tonemapper.py
+++ tonemapper.py
@@ -10,11 +10,13 @@ def generic(x):
     mid_in = 0.18
     mid_out = 0.18
 
-    hdr_max = 16
+    hdr_max = 1
 
-    b = (-math.pow(mid_in, a) + math.pow(hdr_max, a) * mid_out) / (math.pow(math.pow(hdr_max, a), d) - math.pow(math.pow(mid_in, a), d) * mid_out)
+    denom = math.pow(math.pow(hdr_max, a), d) - math.pow(math.pow(mid_in, a), d) * mid_out
+    
+    b = (-math.pow(mid_in, a) + math.pow(hdr_max, a) * mid_out) / denom
 
-    c = (math.pow(math.pow(hdr_max, a), d) * math.pow(mid_in, a) - math.pow(hdr_max, a) * math.pow(math.pow(mid_in, a), d) * mid_out) / (math.pow(math.pow(hdr_max, a), d) - math.pow(math.pow(mid_in, a), d) * mid_out)
+    c = (math.pow(math.pow(hdr_max, a), d) * math.pow(mid_in, a) - math.pow(hdr_max, a) * math.pow(math.pow(mid_in, a), d) * mid_out) / denom
 
     z = math.pow(x, a)
     y = z / (math.pow(z, d) * b + c)
@@ -43,27 +45,28 @@ def aces(x):
     return (x * (a * x + b)) / (x * (c * x + d) + e)
 
 # Plot the tonemapping curves
+plt.figure(figsize=(16, 9), dpi=80)
+
 color_in = []
 color_generic = []
 color_uncharted = []
 color_aces = []
 
 for x in numpy.logspace(-8, 8, num=64, base=2):
-    color = x - 0.00390625
+    color = x - math.pow(2, -8)
     color_in.append(color)
     color_generic.append(generic(color))
     color_uncharted.append(uncharted(color))
     color_aces.append(aces(color))
 
-generic_graph, = plt.semilogx(color_in, color_generic, basex=2, label='Generic')
-uncharted_graph, = plt.plot(color_in, color_uncharted, label='Uncharted')
-aces_graph, = plt.plot(color_in, color_aces, label='ACES')
+plt.semilogx(color_in, color_generic, basex=2, label='Generic')
+plt.plot(color_in, color_uncharted, label='Uncharted')
+plt.plot(color_in, color_aces, label='ACES')
+
 plt.axis([0, 255, 0, 1.4])
-
-plt.legend([generic_graph, uncharted_graph, aces_graph])
-
-plt.xlabel('input')
-plt.ylabel('tonemapped')
+plt.legend()
+plt.xlabel('Input')
+plt.ylabel('Tonemapped')
 plt.grid(True)
 
 plt.show()
