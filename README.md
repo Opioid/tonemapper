c6d9724edccf0e3f95c924ea8e5e39002c6a25a0
Opioid <benjamin.savs@gmail.com>
2016-09-16 21:45:12 +0200

    Updated with Bart's fixes

--- README.md
+++ README.md
@@ -3,8 +3,8 @@
 A python script to debug tonemapping operators.
 It compares the following tonemappers:
 
-1. [Timothy Lottes Generic](http://32ipi028l5q82yhj72224m8j.wpengine.netdna-cdn.com/wp-content/uploads/2016/03/GdcVdrLottes.pdf)
-2. [John Hable "Uncharted" Filmic](http://filmicgames.com/archives/75Jo)
+1. [Timothy Lottes Generic](http://32ipi028l5q82yhj72224m8j.wpengine.netdna-cdn.com/wp-content/uploads/2016/03/GdcVdrLottes.pdf) with [Fixes from Bart Wronski](https://bartwronski.com/2016/09/01/dynamic-range-and-evs/comment-page-1/)
+2. [John Hable "Uncharted" Filmic](http://filmicgames.com/archives/75)
 3. [Krzysztof Narkowicz ACES approximation](https://knarkowicz.wordpress.com/2016/01/06/aces-filmic-tone-mapping-curve/)
 
 ![Tonemapper comparison](https://github.com/Opioid/tonemapper/blob/master/tonemapper.png "Tonemapper comparison")
@@ -15,6 +15,6 @@ The settings for the Generic Tonemapper are:
 - Shoulder = 1
 - Mid in   = 0.18
 - Mit out  = 0.18
-- HDR max  = 1
+- HDR max  = 256
 
-Clearly, there is something wrong here...
+Both the Uncharted and ACES curve have been normalized to 256.
--- tonemapper.py
+++ tonemapper.py
@@ -3,20 +3,19 @@ import math
 import numpy
 
 # Generic like in http://32ipi028l5q82yhj72224m8j.wpengine.netdna-cdn.com/wp-content/uploads/2016/03/GdcVdrLottes.pdf
-def generic(x):
+# but with corrections like in https://bartwronski.com/2016/09/01/dynamic-range-and-evs/comment-page-1/#comment-2360
+def generic(x, hdr_max):
     a = 1 # contrast
     d = 1 # shoulder
 
     mid_in = 0.18
     mid_out = 0.18
 
-    hdr_max = 1
-
-    denom = math.pow(math.pow(hdr_max, a), d) - math.pow(math.pow(mid_in, a), d) * mid_out
+    ad = a * d
     
-    b = (-math.pow(mid_in, a) + math.pow(hdr_max, a) * mid_out) / denom
+    b = -((-math.pow(mid_in,a) + (mid_out*(math.pow(hdr_max,ad)*math.pow(mid_in,a) - math.pow(hdr_max,a)*pow(mid_in,ad)*mid_out))/ (math.pow(hdr_max,ad)*mid_out - math.pow(mid_in,ad)*mid_out))/ (math.pow(mid_in,ad)*mid_out))
 
-    c = (math.pow(math.pow(hdr_max, a), d) * math.pow(mid_in, a) - math.pow(hdr_max, a) * math.pow(math.pow(mid_in, a), d) * mid_out) / denom
+    c = (math.pow(hdr_max, ad) * math.pow(mid_in, a) - math.pow(hdr_max, a) * math.pow(mid_in, ad) * mid_out) / (math.pow(hdr_max,ad) * mid_out - math.pow(mid_in, ad) * mid_out)
 
     z = math.pow(x, a)
     y = z / (math.pow(z, d) * b + c)
@@ -34,6 +33,9 @@ def uncharted(x):
 
     return ((x * (a * x + c * b) + d * e) / (x * (a * x + b) + d * f)) - e / f
 
+def normalized_uncharted(x, hdr_max):
+    return uncharted(x) / uncharted(hdr_max)
+
 # ACES like in https://knarkowicz.wordpress.com/2016/01/06/aces-filmic-tone-mapping-curve/
 def aces(x):
     a = 2.51
@@ -44,6 +46,9 @@ def aces(x):
 
     return (x * (a * x + b)) / (x * (c * x + d) + e)
 
+def normalized_aces(x, hdr_max):
+    return aces(x) / aces(hdr_max)
+
 # Plot the tonemapping curves
 plt.figure(figsize=(12.8, 7.2), dpi=120)
 
@@ -52,20 +57,24 @@ color_generic = []
 color_uncharted = []
 color_aces = []
 
+hdr_max = 256.0
+
 for x in numpy.logspace(-8, 8, num=64, base=2):
     color = x - math.pow(2, -8)
     color_in.append(color)
-    color_generic.append(generic(color))
-    color_uncharted.append(uncharted(color))
-    color_aces.append(aces(color))
+    color_generic.append(generic(color, hdr_max))
+    #color_uncharted.append(uncharted(color))
+    color_uncharted.append(normalized_uncharted(color, hdr_max))
+    #color_aces.append(aces(color))
+    color_aces.append(normalized_aces(color, hdr_max))
 
 plt.semilogx(color_in, color_generic, basex=2, label='Generic')
-plt.plot(color_in, color_uncharted, label='Uncharted')
-plt.plot(color_in, color_aces, label='ACES')
+plt.plot(color_in, color_uncharted, label='Uncharted (normalized)')
+plt.plot(color_in, color_aces, label='ACES (normalized)')
 
 plt.axis([0, 255, 0, 1.4])
 plt.legend()
-plt.xlabel('Input')
+plt.xlabel('Input [0, 256]')
 plt.ylabel('Tonemapped')
 plt.grid(True)
 
