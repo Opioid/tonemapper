edccc5407e787ab8dd6b14a8e49d3709266f620a
Benjamin Savs <Benjamin.SAVS@3ds.com>
2017-04-12 17:22:36 +0200

    Adding linear plot

--- tonemapper.py
+++ tonemapper.py
@@ -67,57 +67,111 @@ def normalized_aces(x, hdr_max):
 def normalized_linear(x, hdr_max):
     return x / hdr_max
 
-# Plot the tonemapping curves
-plt.figure(figsize=(12, 6))
+def plot_linear():
+    # Plot the tonemapping curves
+    plt.figure(figsize=(12, 6))
 
-color_in = []
-color_lottes = []
-color_uncharted = []
-color_aces = []
-color_linear = []
+    color_in = []
+    color_lottes = []
+    color_uncharted = []
+    color_aces = []
+    color_linear = []
 
-hdr_max = 16.0
+    hdr_max = 16.0
 
-for x in numpy.logspace(-1, 5, num=256, base=2):
-    color = x - math.pow(2, -1)
-    color_in.append(color)
-    color_lottes.append(lottes(color, hdr_max))
-    #color_uncharted.append(uncharted(color))
-    color_uncharted.append(normalized_uncharted(color, hdr_max))
-    #color_aces.append(aces(color))
-    color_aces.append(normalized_aces(color, hdr_max))
-    color_linear.append(normalized_linear(color, hdr_max))
+    for x in range(0, 18):
+        color = x
+        color_in.append(color)
+        color_lottes.append(lottes(color, hdr_max))
+        #color_uncharted.append(uncharted(color))
+        color_uncharted.append(normalized_uncharted(color, hdr_max))
+        #color_aces.append(aces(color))
+        color_aces.append(normalized_aces(color, hdr_max))
+        color_linear.append(normalized_linear(color, hdr_max))
 
-plt.semilogx(color_in, color_lottes, basex=2, label='Lottes')
-plt.plot(color_in, color_uncharted, label='Uncharted (normalized)')
-plt.plot(color_in, color_aces, label='ACES (normalized)')
-plt.plot(color_in, color_in, label='Linear (clamped)')
-plt.plot(color_in, color_linear, label='Linear (normalized)')
+    plt.plot(color_in, color_lottes, label='Lottes')
+    plt.plot(color_in, color_uncharted, label='Uncharted (normalized)')
+    plt.plot(color_in, color_aces, label='ACES (normalized)')
+    plt.plot(color_in, color_in, label='Linear (clamped)')
+    plt.plot(color_in, color_linear, label='Linear (normalized)')
 
-plt.axis([0, 19, 0, 1.04])
-plt.legend(loc=2)
-plt.xlabel('Input')
-plt.ylabel('Tonemapped')
+    plt.axis([0, 16.5, 0, 1.04])
+    plt.legend(loc=4)
+    plt.xlabel('Input')
+    plt.ylabel('Tonemapped')
 
-ax = plt.axes()
+    ax = plt.axes()
 
-xlabels = ['', '', '0.015625', '0.03125', '0.0625', '0.125',
-           '0.25', '0.5', '1.0', '2.0', '4.0', '8.0', '16.0']
+    #  xlabels = ['', '', '0.015625', '0.03125', '0.0625', '0.125',
+    #             '0.25', '0.5', '1.0', '2.0', '4.0', '8.0', '16.0']
 
-ax.set_xticklabels(xlabels)
+    #  ax.set_xticklabels(xlabels)
 
-#ax.xaxis.grid()
-ax.tick_params(which='both', # Options for both major and minor ticks
-               direction='out',
-               top='off', # turn off top ticks
-               left='on', # turn off left ticks
-               right='off',  # turn off right ticks
-               bottom='on') # turn off bottom ticks
+    ax.tick_params(which='both', # Options for both major and minor ticks
+                   direction='out',
+                   top='off', # turn off top ticks
+                   left='on', # turn off left ticks
+                   right='off',  # turn off right ticks
+                   bottom='on') # turn off bottom ticks
 
-ax.axhline(1.0,  linestyle='--', color='k')
-ax.axvline(1.0, linestyle=':', color='k')
-ax.axvline(16.0, linestyle='--', color='k')
+    ax.axhline(1.0,  linestyle='--', color='k')
+    ax.axvline(1.0, linestyle=':', color='k')
+    ax.axvline(16.0, linestyle='--', color='k')
 
-# plt.show()                      
+    plt.savefig('tonemapper_linear.png', bbox_inches='tight')
 
-plt.savefig('tonemapper.png', bbox_inches='tight')
+def plot_log():
+    # Plot the tonemapping curves
+    plt.figure(figsize=(12, 6))
+
+    color_in = []
+    color_lottes = []
+    color_uncharted = []
+    color_aces = []
+    color_linear = []
+
+    hdr_max = 16.0
+
+    for x in numpy.logspace(-1, 5, num=256, base=2):
+        color = x - math.pow(2, -1)
+        color_in.append(color)
+        color_lottes.append(lottes(color, hdr_max))
+        #color_uncharted.append(uncharted(color))
+        color_uncharted.append(normalized_uncharted(color, hdr_max))
+        #color_aces.append(aces(color))
+        color_aces.append(normalized_aces(color, hdr_max))
+        color_linear.append(normalized_linear(color, hdr_max))
+
+    plt.semilogx(color_in, color_lottes, basex=2, label='Lottes')
+    plt.plot(color_in, color_uncharted, label='Uncharted (normalized)')
+    plt.plot(color_in, color_aces, label='ACES (normalized)')
+    plt.plot(color_in, color_in, label='Linear (clamped)')
+    plt.plot(color_in, color_linear, label='Linear (normalized)')
+
+    plt.axis([0, 19, 0, 1.04])
+    plt.legend(loc=2)
+    plt.xlabel('Input')
+    plt.ylabel('Tonemapped')
+
+    ax = plt.axes()
+
+    xlabels = ['', '', '0.015625', '0.03125', '0.0625', '0.125',
+               '0.25', '0.5', '1.0', '2.0', '4.0', '8.0', '16.0']
+
+    ax.set_xticklabels(xlabels)
+
+    ax.tick_params(which='both', # Options for both major and minor ticks
+                   direction='out',
+                   top='off', # turn off top ticks
+                   left='on', # turn off left ticks
+                   right='off',  # turn off right ticks
+                   bottom='on') # turn off bottom ticks
+
+    ax.axhline(1.0,  linestyle='--', color='k')
+    ax.axvline(1.0, linestyle=':', color='k')
+    ax.axvline(16.0, linestyle='--', color='k')
+
+    plt.savefig('tonemapper_log.png', bbox_inches='tight')
+
+plot_linear()
+plot_log()
